cb3de736fc88e291cc29888056c2ca2ff68f3533
Bennett Foster <bennfos@gmail.com>
2020-02-17 17:47:50 -0600

    length ternary

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -57,7 +57,7 @@ class Search extends Component {
                         </InputGroup>
                     </div>
                     <div className="results__container">
-                        { this.state.searchResults !== {status: 200} ?
+                        { this.state.searchResults.length > 0 ?
                           this.state.searchResults.map(searchResult => (
                             <SearchResultCard
                                 key={searchResult.id}
